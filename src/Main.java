650cd05304ab7a27bf78b514cae2de0ed89d5ddb
aivhop <chernetsov.alexm@gmail.com>
2023-10-12 00:37:44 +0300

    Small refactoring

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import chernetsov.homework.first.task1.FirstTask;
-import chernetsov.homework.first.task2.SecondTask;
-
-public class Main {
-    public static void main(String[] args) {
-        FirstTask.task();
-        SecondTask.task();
-    }
-}
