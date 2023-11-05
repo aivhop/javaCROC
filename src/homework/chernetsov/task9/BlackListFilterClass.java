7922a8f8d70a3b9efaf34a64f0f2ca05ea1a921a
aivhop <chernetsov.alexm@gmail.com>
2023-11-06 00:37:56 +0300

    ready

--- src/homework/chernetsov/task9/BlackListFilterClass.java
+++ src/homework/chernetsov/task9/BlackListFilterClass.java
@@ -1,24 +1,74 @@
 package homework.chernetsov.task9;
 
-import java.util.*;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
 
 public class BlackListFilterClass implements BlackListFilter {
     @Override
     public void filterComments(List<String> comments, Set<String> blackList) {
         if (comments != null && blackList != null && blackList.size() != 0) {
-            Iterator<String> it = comments.iterator();
+            ListIterator<String> it = comments.listIterator();
             while (it.hasNext()) {
-                String[] words = it.next().split("[\\p{Punct}\s\\p{Cntrl}]+");
-                nextComment:
+                String comment = it.next();
+                if (comment == null) {
+                    it.remove(); // удалим спам
+                    continue;
+                }
+                String[] words = comment.split("[\\s\\p{Punct}\\p{Cntrl}]+");
                 for (String word : words) {
                     for (String blackWord : blackList) {
-                        if (blackWord.equalsIgnoreCase(word)) {
-                            it.remove();
-                            break nextComment;
+                        if (blackWord != null && isEqualOrMisspellIgnoreCase(word, blackWord)) {
+                            comment = modifyComment(comment,word);
+                            it.set(comment);
+                            break;
                         }
                     }
                 }
             }
         }
+    }
+
+    private String modifyComment(String comment, String word) {
+        StringBuilder refactorComment = new StringBuilder(comment);
+        int ind = refactorComment.indexOf(word);
+        refactorComment.replace(ind, ind + word.length(), "*".repeat(word.length()));
+        return refactorComment.toString();
+    }
+
+    private boolean isEqualOrMisspellIgnoreCase(String word1, String word2) {
+        word1 = word1.toLowerCase();
+        word2 = word2.toLowerCase();
+        return word1.equals(word2) || isMisspell(word1, word2);
+    }
+
+    private boolean isMisspell(String word1, String word2) {
+        int differenceLength = Math.abs(word1.length() - word2.length());
+        if (differenceLength < 2) {
+            int countCriticalMisses = differenceLength;
+            if (differenceLength == 1) {
+                // если разница равна единице, возможно пользователь пропустил первую букву в слове
+                // Example: ошка - кошка
+                if (word1.length() > word2.length() && word1.substring(1).equals(word2) ||
+                        word2.substring(1).equals(word1)) {
+                    return true;
+                }
+            }
+            for (int i = 0; i < Math.min(word1.length(), word2.length()) && countCriticalMisses < 2; i++) {
+                if (word1.charAt(i) != word2.charAt(i)) { // если несовпадение
+                    if (countCriticalMisses == 1 && differenceLength == 0) {
+                        // если уже было несовпадение, а длины равны, возможно перепутан порядок двух букв,
+                        // тогда нужно попробовать сравнить символ каждого слова с предыдущим другого
+                        if (word1.charAt(i) == word2.charAt(i - 1) && word2.charAt(i) == word1.charAt(i - 1)) {
+                            // тогда не нужно ничего делать, эта ошибка уже посчитана
+                            continue;
+                        }
+                    }
+                    countCriticalMisses++;
+                }
+            }
+            return countCriticalMisses == 1; // истина если есть только 1 ошибка
+        }
+        return false;
     }
 }
--- src/homework/chernetsov/task9/ShowTask.java
+++ src/homework/chernetsov/task9/ShowTask.java
@@ -12,17 +12,13 @@ public class ShowTask {
         List<String> comments = new ArrayList<>();
         comments.add("Кот, собака, попугай?");
         comments.add("Кошка");
+
         Set<String> blackList = new HashSet<>();
         blackList.add("кот");
         blackList.add("Кошка");
         blackList.add("котенок");
         blackList.add("котик");
-        myFilter.filterComments(comments,blackList);
-        for(String comment: comments){
-            System.out.println(comment);
-        }
-        comments.clear();
-
+        blackList.add(null);
 
         comments.add("Допустим это первый комментарий, содержащий blackWord_+_+)_+( пусть это будет слово КОШКА");
         comments.add("А это второй комментарий, который не содержит такое слово");
@@ -34,8 +30,19 @@ public class ShowTask {
         comments.add("А если добавить еще перенос строки,\n то считывание слов должно быть корректным");
         comments.add("А если добавить еще перенос строки,\nКошка\n");
         comments.add("А если добавить еще перенос строки,\n\n\tКоШкА\t ");
+        comments.add(null);
+        comments.add("А это проверка опечаток. ког");
+        comments.add("А это проверка опечаток. котт");
+        comments.add("И это лошка");
+        comments.add("И это ко");
+        comments.add("И это коетнок");
+        comments.add("И это ошка");
+        comments.add("");
+        comments.add("..");
+        comments.add("А если некорректных слов несколько, кот например и кошка");
 
         myFilter.filterComments(comments,blackList);
+
         for(String comment: comments){
             System.out.println(comment);
         }
