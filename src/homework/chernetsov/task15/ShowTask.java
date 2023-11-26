e26f0c03b61bb91128dd134dd4417b1fe85cc24a
aivhop <chernetsov.alexm@gmail.com>
2023-11-27 01:42:10 +0300

    task15

--- src/homework/chernetsov/task15/ShowTask.java
+++ src/homework/chernetsov/task15/ShowTask.java
@@ -3,18 +3,18 @@ package homework.chernetsov.task15;
 import homework.chernetsov.task15.dbservice.DBConnection;
 import homework.chernetsov.task15.dbservice.DBFilling;
 import homework.chernetsov.task15.dbservice.FileProcessing;
-import homework.chernetsov.task15.exceptions.InvalidResourceFileName;
 
 import java.io.BufferedReader;
 
 
 public class ShowTask {
     public static void main(String[] args) {
-        if (args.length != 1) {
+        if (args.length == 0) {
             throw new IllegalArgumentException("Sorry, we need file path");
         }
         try (BufferedReader reader = FileProcessing.checkAndReadFileCSV(args[0]);
-             DB db = new DB(DBConnection.tryConnect().getConnection())) {
+             DBConnection con = DBConnection.tryConnect();
+             DB db = new DB(con.getConnection())) {
             DBFilling.fill(db, reader);
             System.out.println(db);
         } catch (Exception e) {
